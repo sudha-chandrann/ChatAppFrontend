0edb6acb45db798d34c6fc9e7a2f53c41a55f759
sudhachandrann <sudhachandran1020@gmail.com>
2025-04-08 12:58:12 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+  
+      <div className='h-screen flex items-center justify-center bg-black'>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+       
+      </div>
+
+  
+  )
+}
+
+export default App
